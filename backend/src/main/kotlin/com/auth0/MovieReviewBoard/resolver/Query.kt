aa15e9dd7ae12a70b6ba5003864fbb914a1ec0ab
theerasun_tangngamchit <theerasun@gmail.com>
2019-08-28 08:21:31 +0700

    -add spqr

--- backend/src/main/kotlin/com/auth0/MovieReviewBoard/resolver/Query.kt
+++ backend/src/main/kotlin/com/auth0/MovieReviewBoard/resolver/Query.kt
@@ -6,13 +6,10 @@ import com.auth0.MovieReviewBoard.movie.Movie
 import com.auth0.MovieReviewBoard.movie.MovieRepository
 import com.coxautodev.graphql.tools.GraphQLQueryResolver
 import io.leangen.graphql.annotations.GraphQLArgument
-import io.leangen.graphql.annotations.GraphQLQuery
-import io.leangen.graphql.spqr.spring.annotations.GraphQLApi
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
-@GraphQLApi
 class Query(val movieRepository: MovieRepository, val directorRepository: DirectorRepository) : GraphQLQueryResolver {
 
     fun findAllMovies(): Iterable<Movie> {
@@ -41,22 +38,18 @@ class Query(val movieRepository: MovieRepository, val directorRepository: Direct
         return movieRepository.countByDirector(director.get())
     }
 
-    @GraphQLQuery(name = "directors")
     fun getDirectors(): MutableIterable<Director> {
         return directorRepository.findAll();
     }
 
-    @GraphQLQuery(name = "director")
     fun getDirector(@GraphQLArgument(name = "id") id: Long): Optional<Director> {
         return directorRepository.findById(id);
     }
 
-    @GraphQLQuery(name = "movies")
     fun getMovies(): MutableIterable<Movie> {
         return movieRepository.findAll();
     }
 
-    @GraphQLQuery(name = "movie")
     fun getMovie(@GraphQLArgument(name = "id") id: Long): Optional<Movie> {
         return movieRepository.findById(id);
     }
